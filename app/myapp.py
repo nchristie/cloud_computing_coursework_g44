2079ef07e10ea1629da8715a19ab880b84e745cf
Naomi Christie <naomi.christie@fundingcircle.com>
2022-04-11 02:22:15 +0100

    Tidy up code, fix import for requests

--- app/myapp.py
+++ app/myapp.py
@@ -1,60 +1,56 @@
 # imports
 from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
- 
+import requests
+
 # initializing Flask app
 app = Flask(__name__)
- 
+
 # Google Cloud SQL (change this accordingly)
 PASSWORD ="0000"
 PUBLIC_IP_ADDRESS ="35.242.173.183"
 DBNAME ="gcp_project"
 PROJECT_ID ="animated-rope-339716"
 INSTANCE_NAME ="gcp-project"
- 
+
 # configuration
 app.config["SECRET_KEY"] = "yoursecretkey"
 app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
- 
+
 db = SQLAlchemy(app)
- 
+
 # User ORM for SQLAlchemy
 class Albumsongs(db.Model):
     albumname = db.Column(db.String(255), primary_key = True, nullable = False)
     songname = db.Column(db.String(255),primary_key = True, nullable = False)
-
-
 class Bandalbums(db.Model):
     bandname = db.Column(db.String(255), primary_key = True, nullable = False)
     albumname = db.Column(db.String(255), primary_key = True, nullable = False)
     description = db.Column(db.String(255),primary_key = False, nullable = True)
     releaseyear = db.Column(db.Integer,primary_key = False, nullable = True)
 
-
 @app.route('/', methods = ['GET'])
 def hello():
     return('<h1>Welcome to the Music Site!<h1>')
- 
- 
-@app.route('/records/')
+
+@app.route('/records/', methods = ['GET'])
 def view():
     # fetches all the songs in all albums
     songs = Albumsongs.query.all()
     # response list consisting albums and songs
     response = list()
- 
+
     for song in songs:
         response.append({
             "albumname" : song.albumname,
             "songname": song.songname
         })
- 
+
     return make_response({
         'status' : 'success',
         'message': response
     }, 200)
-
 
 @app.route('/records/video/<bandname>', methods=['GET'])
 def get_video(bandname):
@@ -62,19 +58,12 @@ def get_video(bandname):
     try:
         formatted_bandname = "+".join(bandname.split())
         itunes_request = f"https://itunes.apple.com/search?term={formatted_bandname}&entity=musicVideo&limit=1"
-
         itunes_response_json = requests.get(itunes_request).json()
         itunes_url = itunes_response_json['results'][0]["previewUrl"]
         itunes_url_with_quotes = f'"{itunes_url}"'
         return(f"<h1>Here's a great video by {bandname}!</h1><h2><a href={itunes_url_with_quotes}>{itunes_url}</a></h2>")
-
-
     except:
         return(f"<h1>Sorry, we couldn't find a video by {bandname} for you :(</h1>")
-
-
-
-
 
 @app.route('/addbandalbum/', methods =['POST', 'GET'])
 def add():
@@ -84,7 +73,6 @@ def add():
     description = request.form.get('description')
     releaseyear = request.form.get('releaseyear')
 
- 
     try:
         # creating Bandalbums object
         bandalbum = Bandalbums(bandname = bandname, albumname = albumname, description = description, releaseyear = releaseyear)
@@ -97,23 +85,16 @@ def add():
             'status' : 'success',
             'message': 'Successfully Added.'
         }
- 
+
         return make_response(responseObject, 200)
     except:
         responseObject = {
             'status' : 'fail',
             'message': 'Some error occured !!'
         }
- 
+
         return make_response(responseObject, 400)
 
-
-
-
-
-
- 
- 
 if __name__ == "__main__":
     # serving the app directly
     app.run(host='0.0.0.0', port =80)
